44382001a5c2c3e932a5ac174e2685c3b3d1fec4
Marc A. Valle <marc.a.valle@intel.com>
2020-10-13 10:32:23 -0700

    Fail windows build as soon as error detected
    For some stages of build script further cmake commands would be invoked
    instead of failing, making failure confusing without deep review of
    logs, and returning 0.

--- script/build.bat
+++ script/build.bat
@@ -30,7 +30,7 @@ GOTO Loop
 set CMAKE_BINARY_DIR=_build
 if defined VPL_INSTALL_DIR (
    call "%VPL_INSTALL_DIR%\env\vars.bat" || exit /b 1
-   set INSTALL_OPTS=-DCMAKE_INSTALL_PREFIX=%VPL_INSTALL_DIR%   
+   set INSTALL_OPTS=-DCMAKE_INSTALL_PREFIX=%VPL_INSTALL_DIR%
 )
 if "%USE_GPL%"=="yes" (
   set GPL_OPTS=-DBUILD_GPL_X264=ON
@@ -42,12 +42,13 @@ cd %CMAKE_BINARY_DIR%
 :: work around parallel build bug
 mkdir %BUILD_MODE%
 
-cmake -A x64 %INSTALL_OPTS% %GPL_OPTS% -DCMAKE_BUILD_TYPE=%BUILD_MODE% .. 
+cmake -A x64 %INSTALL_OPTS% %GPL_OPTS% -DCMAKE_BUILD_TYPE=%BUILD_MODE% .. ^
+      || exit /b 1
 
-cmake --build . --config %BUILD_MODE% -j %NUMBER_OF_PROCESSORS%
+cmake --build . --config %BUILD_MODE% -j %NUMBER_OF_PROCESSORS% || exit /b 1
 
-cmake --build . --config %BUILD_MODE% --target package
- 
+cmake --build . --config %BUILD_MODE% --target package || exit /b 1
+
 if defined TEAMCITY_VERSION (
    echo ##teamcity[publishArtifacts 'oneVPL-cpu/%CMAKE_BINARY_DIR%/*-all.zip=^>']
 )
