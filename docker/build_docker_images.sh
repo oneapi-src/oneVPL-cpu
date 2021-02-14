f46bc287cbd4f7438d96ac76e460ccbf561fd958
wd13384 <78171734+wd13384@users.noreply.github.com>
2021-02-13 20:10:02 -0800

    Update build_docker_images.sh

--- docker/build_docker_images.sh
+++ docker/build_docker_images.sh
@@ -7,10 +7,10 @@
 
 set -e
 BASEDIR=$(dirname "$0")
-#docker build -f ${BASEDIR}/Dockerfile-centos-7-GPL -t vpl-cpu:centos7-GPL ${BASEDIR}/..
-#docker build -f ${BASEDIR}/Dockerfile-centos-7     -t vpl-cpu:centos7     ${BASEDIR}/..
-#docker build -f ${BASEDIR}/Dockerfile-centos-8     -t vpl-cpu:centos8     ${BASEDIR}/..
-#docker build -f ${BASEDIR}/Dockerfile-ubuntu-18.04 -t vpl-cpu:ubuntu18.04 ${BASEDIR}/..
+docker build -f ${BASEDIR}/Dockerfile-centos-7-GPL -t vpl-cpu:centos7-GPL ${BASEDIR}/..
+docker build -f ${BASEDIR}/Dockerfile-centos-7     -t vpl-cpu:centos7     ${BASEDIR}/..
+docker build -f ${BASEDIR}/Dockerfile-centos-8     -t vpl-cpu:centos8     ${BASEDIR}/..
+docker build -f ${BASEDIR}/Dockerfile-ubuntu-18.04 -t vpl-cpu:ubuntu18.04 ${BASEDIR}/..
 
 if [ "$(uname -m)" == "x86_64" ]; then
 	docker build -f ${BASEDIR}/Dockerfile-ubuntu-19.10 -t vpl-cpu:ubuntu19.10 ${BASEDIR}/..
