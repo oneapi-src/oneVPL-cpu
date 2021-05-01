b66062076a458195fd2e26b71daa07053a73d736
Marc A. Valle <marc.a.valle@intel.com>
2021-04-30 18:41:03 -0700

    Fix test scripts to account for FHS instal layout

--- script/test.bat
+++ script/test.bat
@@ -1,12 +1,12 @@
 @REM ------------------------------------------------------------------------------
 @REM Copyright (C) Intel Corporation
-@REM 
+@REM
 @REM SPDX-License-Identifier: MIT
 @REM ------------------------------------------------------------------------------
 @REM Run basic tests on base.
 
 @ECHO off
-SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION 
+SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 
 @REM Read command line options
 CALL %~dp0%\_buildopts.bat ^
@@ -16,42 +16,40 @@ CALL %~dp0%\_buildopts.bat ^
 IF DEFINED HELP_OPT ( EXIT /b 0 )
 
 @REM Load project environment
-SET VARS_SCRIPT=vars.bat
-IF "%ARCH_OPT%"=="x86_32" (
-  SET VARS_SCRIPT=vars32.bat
+set VPL_INTEL_ARCH=intel64
+if "%ARCH_OPT%"=="x86_32" (
+  set VPL_INTEL_ARCH=ia32
 )
 
-IF NOT DEFINED "%VPL_ROOT%" (
-  IF NOT EXIST "%VPL_INSTALL_DIR%" (
-    ECHO "Base must be installed to build implementation"
-    EXIT /b 1
-  )
-
-  IF NOT EXIST "%VPL_INSTALL_DIR%\env\%VARS_SCRIPT%" (
-    @REM Detect case where user points VPL_INSTALL_DIR at the
-    @REM Base repo instead of the built output.
-    ECHO "Cannot find environment script in %VPL_INSTALL_DIR%\env"
-    EXIT /b 1
-  )
-
-  CALL "%VPL_INSTALL_DIR%\env\%VARS_SCRIPT%" || EXIT /b 1
+:: do custom environment configuration if var is set.
+if defined VPL_INSTALL_DIR (
+   echo VPL_INSTALL_DIR set, assuming environment script needs to be run.
+   if exist "%VPL_INSTALL_DIR%\share\oneVPL\env" (
+      echo Using custom environment configuration from %VPL_INSTALL_DIR%\share\oneVPL\env
+      call "%VPL_INSTALL_DIR%\share\oneVPL\env\vars.bat" %VPL_INTEL_ARCH% || exit /b 1
+   ) else (
+      echo Using custom environment configuration from %VPL_INSTALL_DIR%\env
+      call "%VPL_INSTALL_DIR%\env\vars.bat" %VPL_INTEL_ARCH% || exit /b 1
+   )
 )
 
 set /A result_all = 0
-SET BUILD_DIR=_build
-PUSHD  %BUILD_DIR%
-  PUSHD %COFIG_OPT%
-    ECHO *** Running Unit Tests ***
-    CALL vpl-utest.exe --gtest_output=xml:%PROJ_DIR%\_logs\
-    IF %errorlevel%==0 GOTO unit_tests_passed
-    ECHO *** Unit Tests FAILED ***
-    SET /A result_all = 1
-    GOTO test_end
+PUSHD %PROJ_DIR%
+  SET BUILD_DIR=_build
+  PUSHD  %BUILD_DIR%
+    PUSHD %COFIG_OPT%
+      ECHO *** Running Unit Tests ***
+      CALL vpl-utest.exe --gtest_output=xml:%PROJ_DIR%\_logs\
+      IF %errorlevel%==0 GOTO unit_tests_passed
+      ECHO *** Unit Tests FAILED ***
+      SET /A result_all = 1
+      GOTO test_end
 
-    :unit_tests_passed
-    echo *** Unit Tests PASSED ***
+      :unit_tests_passed
+      echo *** Unit Tests PASSED ***
 
-    :test_end
+      :test_end
+    POPD
   POPD
 POPD
 
