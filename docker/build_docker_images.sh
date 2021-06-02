58797118443c92d7b157d395f916a65b99ebcb8d
Thaddeus Letnes <thaddeus.c.letnes@intel.com>
2021-06-02 11:01:29 -0700

    Fix Docker build (#26)

--- docker/build_docker_images.sh
+++ docker/build_docker_images.sh
@@ -7,9 +7,13 @@
 
 set -e
 BASEDIR=$(dirname "$0")
+echo "Building CentOS 7 GPL Image"
 docker build -f ${BASEDIR}/Dockerfile-centos-7-GPL -t vpl-cpu:centos7-GPL ${BASEDIR}/..
+echo "Building CentOS 7 Image"
 docker build -f ${BASEDIR}/Dockerfile-centos-7     -t vpl-cpu:centos7     ${BASEDIR}/..
+echo "Building CentOS 8 Image"
 docker build -f ${BASEDIR}/Dockerfile-centos-8     -t vpl-cpu:centos8     ${BASEDIR}/..
+echo "Building Ubuntu 18.04 Image"
 docker build -f ${BASEDIR}/Dockerfile-ubuntu-18.04 -t vpl-cpu:ubuntu18.04 ${BASEDIR}/..
-docker build -f ${BASEDIR}/Dockerfile-ubuntu-19.10 -t vpl-cpu:ubuntu19.10 ${BASEDIR}/..
+echo "Building Ubuntu 20.04 Image"
 docker build -f ${BASEDIR}/Dockerfile-ubuntu-20.04 -t vpl-cpu:ubuntu20.04 ${BASEDIR}/..
