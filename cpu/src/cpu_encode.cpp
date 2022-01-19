0a092ad67d6bc230a4a4c64c02d9b084f3894d8d
Kelvin Hu <kelvin.hu@intel.com>
2022-01-19 09:11:17 +0800

    Support no B-frame cases in SVT-HEVC encoder

--- cpu/src/cpu_encode.cpp
+++ cpu/src/cpu_encode.cpp
@@ -701,6 +701,10 @@ mfxStatus CpuEncode::InitHEVCParams(mfxVideoParam *par) {
 
         m_avEncContext->bit_rate = par->mfx.TargetKbps * 1000; // prop is in kbps;
     }
+    // GopRefDist is distance between I- or P- key frames (1 means no B-frames or IPPP)
+    if (par->mfx.GopRefDist == 1) {
+        av_opt_set_int(m_avEncContext->priv_data, "pred_struct", 0, AV_OPT_SEARCH_CHILDREN);
+    }
 
     if (par->mfx.TargetUsage) {
         // set targetUsage
