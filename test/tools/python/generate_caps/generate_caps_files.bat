24599321a82be17c79f273add3dc160d3ba1a4aa
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-20 21:24:38 +0800

    Delete test directory

--- test/tools/python/generate_caps/generate_caps_files.bat
+++
@@ -1,5 +0,0 @@
-call python gencaps.py dec caps_decode.csv ..\..\..\..\cpu\src\libmfxvplsw_caps_dec.h 
-call python gencaps.py enc caps_encode.csv ..\..\..\..\cpu\src\libmfxvplsw_caps_enc.h
-call python gencaps.py vpp caps_vpp.csv ..\..\..\..\cpu\src\libmfxvplsw_caps_vpp.h
-
-call python gencaps.py enc caps_encode_h264.csv ..\..\..\..\cpu\src\libmfxvplsw_caps_enc_h264.h
--- test/tools/python/generate_caps/generate_caps_files.sh
+++
@@ -1,5 +0,0 @@
-python gencaps.py dec caps_decode.csv ../../../../cpu/src/libmfxvplsw_caps_dec.h 
-python gencaps.py enc caps_encode.csv ../../../../cpu/src/libmfxvplsw_caps_enc.h
-python gencaps.py vpp caps_vpp.csv ../../../../cpu/src/libmfxvplsw_caps_vpp.h
-
-python gencaps.py enc caps_encode_h264.csv ../../../../cpu/src/libmfxvplsw_caps_enc_h264.h
