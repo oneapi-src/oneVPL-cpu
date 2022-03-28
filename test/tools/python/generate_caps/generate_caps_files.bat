2538345ecf99602294475542a7d2dff25de7b0ca
Jon Recker <jon.recker@intel.com>
2022-03-28 14:22:23 -0700

    Update encode capabilities structs for API 2.7

--- test/tools/python/generate_caps/generate_caps_files.bat
+++ test/tools/python/generate_caps/generate_caps_files.bat
@@ -2,4 +2,5 @@ call python gencaps.py dec caps_decode.csv ..\..\..\..\cpu\src\libmfxvplsw_caps_
 call python gencaps.py enc caps_encode.csv ..\..\..\..\cpu\src\libmfxvplsw_caps_enc.h
 call python gencaps.py vpp caps_vpp.csv ..\..\..\..\cpu\src\libmfxvplsw_caps_vpp.h
 
-call python gencaps.py enc caps_encode_h264.csv ..\..\..\..\cpu\src\libmfxvplsw_caps_enc_h264.h
+call python gencaps.py enc caps_encode_x264.csv ..\..\..\..\cpu\src\libmfxvplsw_caps_enc_x264.h
+call python gencaps.py enc caps_encode_openh264.csv ..\..\..\..\cpu\src\libmfxvplsw_caps_enc_openh264.h
